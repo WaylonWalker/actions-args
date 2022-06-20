664fbfce382a6ca9e9e9cedcbd724fabc7781eb8
Waylon S. Walker <waylon@waylonwalker.com>
2022-06-19 22:09:05 -0500

    install specific version and run

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 # `$*` expands the `args` supplied in an `array` individually
 # or splits `args` in a string separated by whitespace.
-sh -c "echo $*"
+if [[ -z $1 ]]
+then
+    pip install markata
+else
+    pip install markata==$1
+fi
+
+markata build
+
